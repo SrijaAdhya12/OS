4dc97f71bb9498d26946e9e59d0809196e02dc96
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-11-17 21:18:13 +0530

    Update maximum.sh

--- assignment/maximum.sh
+++ assignment/maximum.sh
@@ -13,3 +13,17 @@ elif [[ $b -gt $c ]]; then
 else
         echo "$c"
 fi
+
+#or 
+num1=$1
+num2=$2
+num3=$3
+
+if [ $num1 -ge $num2 ] && [ $num1 -ge $num3 ]; then
+        max=$num1
+elif [ $num2 -ge $num1 ] && [ $num2 -ge $num3 ]; then
+        max=$num2
+else
+        max=$num3
+fi
+echo "The maximum number is: $max"
