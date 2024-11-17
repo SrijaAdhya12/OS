708a47007d02b8a6d1006bf8981075d9b9f94dc6
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-11-17 21:05:42 +0530

    Update leapyear.sh

--- assignment/leapyear.sh
+++ assignment/leapyear.sh
@@ -3,3 +3,16 @@
 year=${1:-"$(date +%Y)"}
 
 ((!(year % 4))) && ( ((!(year % 400))) || ((year % 100))) && echo Leap Year || echo Not a Leap Year
+
+#or
+
+if [ -z "$1"]; then
+        year=$(date +%Y)
+else
+        year=$1
+fi
+if (( $year % 4 == 0 && year % 100 != 0 )) || ( $year % 400 == 0 ); then
+        echo "$year is a Leap Year"
+else
+        echo "$year is not a leap year"
+fi
